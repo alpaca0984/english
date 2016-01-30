57690640c40140a2c4c79a18e9a697e61a033c33
Masataka Hirano <hirano.masataka@a-tm.co.jp>
2016-01-30 03:59:16 +0000

    500_error

--- app/controllers/dictates_controller.rb
+++ app/controllers/dictates_controller.rb
@@ -2,4 +2,8 @@ class DictatesController < ApplicationController
   def play
     gon.sentences = Sentence.limit(1000)
   end
+
+  def error
+    head 502
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   get '/' => 'dictates#play'
   get '/dictates/' => 'dictates#play'
+  get '/error/' => 'dictates#error' 
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
